f28f88fa9c82aed782a1b49373da43b1691054a2
= <=duongquocanh368@gmail.com>
2022-10-13 22:44:22 +0700

    DOTA

---
+++ c++/random.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib> // for rand() and srand()
+#include <ctime> // for time()
+#include <windows.h>
+using namespace std;
+
+int main(){
+	system("cls");
+	int a;
+	a=rand(); 
+
+	
+ cout<<a;
+	return 0;
+}
